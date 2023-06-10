730bd451862f64201de50483aed4d25ed0dff4e8
Andrzej Jaromin <ajaromin@silenteight.com>
2023-06-10 07:02:29 +0200

    Initial commit, empty project with Spring Web

---
+++ src/main/java/com/entitylogic/github/extractor/GithubExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.entitylogic.github.extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entitylogic/github/extractor/GithubExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entitylogic.github.extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
